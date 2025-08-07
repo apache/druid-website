8cda19a194ca06319371f5630cd8dbdf3c8a8e3e
capistrant <lucas.capistrant@imply.io>
2025-08-06 19:59:28 -0500

    d34-rc2 docs

--- js/version.js
+++ js/version.js
@@ -28,7 +28,7 @@ Used by
 const Releases = [
   {
     version: "34.0.0",
-    date: "Jul 28 2025",
+    date: "Aug 06 2025",
   },
   {
     version: "33.0.0",
@@ -37,7 +37,7 @@ const Releases = [
   {
     version: "32.0.1",
     date: "Mar 19 2025",
-  },
+  }
 ]
 
 module.exports.Releases = Releases;
